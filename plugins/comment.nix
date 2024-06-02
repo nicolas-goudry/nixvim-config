6d277fcd667238afe45b578c15b3e17a15df2451
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-06-02 22:45:29 +0200

    feat(plugins): add comment.nvim and ts-context-commentstring

---
+++ plugins/comment.nix
@@ -0,0 +1,13 @@
+# homepage: https://github.com/numtostr/comment.nvim/
+# nixvim doc: https://nix-community.github.io/nixvim/plugins/comment/index.html
+_:
+
+{
+  opts = {
+    enable = true;
+
+    settings.pre_hook = ''
+      require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+    '';
+  };
+}
---
+++ plugins/ts-context-commentstring.nix
@@ -0,0 +1,7 @@
+# homepage: https://github.com/JoosepAlviste/nvim-ts-context-commentstring
+# nixvim doc: https://nix-community.github.io/nixvim/plugins/ts-context-commentstring.html
+_:
+
+{
+  opts.enable = true;
+}

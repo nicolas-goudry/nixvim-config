2ad46396b90549d555e237d351cc79bd389c89cd
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-06-02 22:00:11 +0200

    feat(plugins): add nvim-highlight-colors

---
+++ plugins/nvim-highlight-colors.nix
@@ -0,0 +1,26 @@
+# homepage: https://github.com/brenoprata10/nvim-highlight-colors
+{ icons, pkgs, ... }:
+
+{
+  extra = {
+    packages = [ pkgs.vimPlugins.nvim-highlight-colors ];
+
+    config = ''
+      require("nvim-highlight-colors").setup({
+        render = "virtual",
+        virtual_symbol = "${icons.FileModified}",
+        virtual_symbol_position = "eow",
+        virtual_symbol_prefix = " ",
+        virtual_symbol_suffix = "",
+      })
+    '';
+  };
+
+  rootOpts.keymaps = [
+    {
+      key = "<leader>uz";
+      action = "<cmd>HighlightColors Toggle<cr>";
+      options.desc = "Toggle color highlight";
+    }
+  ];
+}

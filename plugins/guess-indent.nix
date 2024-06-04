61d8dd64fe67ed2294872c92a7c6531168e966ba
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-06-04 07:36:32 +0200

    feat(plugins): add guess-indent

---
+++ plugins/guess-indent.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+{
+  extra = {
+    packages = [ pkgs.vimPlugins.guess-indent-nvim ];
+
+    # https://github.com/AstroNvim/AstroNvim/blob/v4.7.7/lua/astronvim/plugins/configs/guess-indent.lua
+    config = ''
+      require("guess-indent").setup({})
+      vim.cmd.lua { args = { "require('guess-indent').set_from_buffer('auto_cmd')" }, mods = { silent = true } }
+    '';
+  };
+}

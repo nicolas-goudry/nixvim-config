454ba629d88794f2abeac038791016966724641c
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-06-04 20:59:14 +0200

    refactor(plugins): prefer single line lua functions where possible

--- plugins/comment.nix
+++ plugins/comment.nix
@@ -5,9 +5,6 @@ _:
 {
   opts = {
     enable = true;
-
-    settings.pre_hook = ''
-      require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
-    '';
+    settings.pre_hook = "require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()";
   };
 }
--- plugins/gitsigns.nix
+++ plugins/gitsigns.nix
@@ -29,92 +29,52 @@
     keymaps = [
       {
         key = "]g";
-        action.__raw = ''
-          function()
-            require("gitsigns").next_hunk()
-          end
-        '';
+        action.__raw = "function() require('gitsigns').next_hunk() end";
         options.desc = "Next Git hunk";
       }
       {
         key = "[g";
-        action.__raw = ''
-          function()
-            require("gitsigns").prev_hunk()
-          end
-        '';
+        action.__raw = "function() require('gitsigns').prev_hunk() end";
         options.desc = "Previous Git hunk";
       }
       {
         key = "<leader>gl";
-        action.__raw = ''
-          function()
-            require("gitsigns").blame_line { full = true }
-          end
-        '';
+        action.__raw = "function() require('gitsigns').blame_line { full = true } end";
         options.desc = "View full Git blame";
       }
       {
         key = "<Leader>gp";
-        action.__raw = ''
-          function()
-            require("gitsigns").preview_hunk_inline()
-          end
-        '';
+        action.__raw = "function() require('gitsigns').preview_hunk_inline() end";
         options.desc = "Preview Git hunk";
       }
       {
         key = "<Leader>gh";
-        action.__raw = ''
-          function()
-            require("gitsigns").reset_hunk()
-          end
-        '';
+        action.__raw = "function() require('gitsigns').reset_hunk() end";
         options.desc = "Reset Git hunk";
       }
       {
         key = "<Leader>gr";
-        action.__raw = ''
-          function()
-            require("gitsigns").reset_buffer()
-          end
-        '';
+        action.__raw = "function() require('gitsigns').reset_buffer() end";
         options.desc = "Reset Git buffer";
       }
       {
         key = "<Leader>gs";
-        action.__raw = ''
-          function()
-            require("gitsigns").stage_hunk()
-          end
-        '';
+        action.__raw = "function() require('gitsigns').stage_hunk() end";
         options.desc = "Stage Git hunk";
       }
       {
         key = "<Leader>gS";
-        action.__raw = ''
-          function()
-            require("gitsigns").stage_buffer()
-          end
-        '';
+        action.__raw = "function() require('gitsigns').stage_buffer() end";
         options.desc = "Stage Git buffer";
       }
       {
         key = "<Leader>gu";
-        action.__raw = ''
-          function()
-            require("gitsigns").undo_stage_hunk()
-          end
-        '';
+        action.__raw = "function() require('gitsigns').undo_stage_hunk() end";
         options.desc = "Unstage Git hunk";
       }
       {
         key = "<Leader>gd";
-        action.__raw = ''
-          function()
-            require("gitsigns").diffthis()
-          end
-        '';
+        action.__raw = "function() require('gitsigns').diffthis() end";
         options.desc = "View Git diff";
       }
     ];

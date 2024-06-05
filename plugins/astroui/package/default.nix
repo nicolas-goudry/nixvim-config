42d32eb2e4a7da014e2fab8d630fa19165a26ef2
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-06-05 14:45:25 +0200

    feat(plugins): add astroui

---
+++ plugins/astroui/package/default.nix
@@ -0,0 +1,17 @@
+{ lib, pkgs, ... }:
+
+let
+  version = "2.1.4";
+in
+pkgs.vimUtils.buildVimPlugin {
+  inherit version;
+
+  name = "astroui";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "astronvim";
+    repo = "astroui";
+    rev = "v${version}";
+    hash = "sha256-nmcqJq4L6XFrgrORan5x+WCwSfU3FC4D6Zux45YnIUQ=";
+  };
+}

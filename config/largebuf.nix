888c7cf47958f214e05845b8d1830af046464a82
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-06-06 07:55:10 +0200

    feat(config): disable wrap and list chars on large buffers

--- config/largebuf.nix
+++ config/largebuf.nix
@@ -1,6 +1,7 @@
 {
   # Adapted from AstroNvim and Reddit comment
   # https://github.com/AstroNvim/astrocore/blob/v1.5.0/lua/astrocore/init.lua#L473-L486
+  # https://github.com/AstroNvim/AstroNvim/blob/v4.7.7/lua/astronvim/plugins/_astrocore_autocmds.lua#L218-L238
   # https://www.reddit.com/r/neovim/comments/z85s1l/comment/iyfrgvb/
   autoGroups.large_buffer_detector = { };
   autoCmd = [
@@ -19,6 +20,8 @@
             vim.cmd("LspStop")
             vim.opt_local.foldmethod = "manual"
             vim.opt_local.spell = false
+            vim.opt_local.wrap = true
+            vim.opt_local.list = false
           end
         end
       '';

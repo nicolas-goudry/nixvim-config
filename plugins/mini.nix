69324f6df456063936dc607ecd9bb2dd8196abf1
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-06-09 09:44:51 +0200

    refactor(plugins/mini): use nixvim mini with bufremove module

---
+++ plugins/mini.nix
@@ -0,0 +1,12 @@
+# homepage: https://github.com/echasnovski/mini.nvim
+# nixvim doc: https://nix-community.github.io/nixvim/plugins/mini.html
+_:
+
+{
+  opts = {
+    enable = true;
+
+    # https://github.com/echasnovski/mini.bufremove
+    modules.bufremove = { };
+  };
+}

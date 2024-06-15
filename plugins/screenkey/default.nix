4371dfbdc0127f79529e1c9cbcab60ec7b17077c
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-06-16 00:17:11 +0200

    feat(plugins/screenkey): add keymap to toggle screenkey

--- plugins/screenkey/default.nix
+++ plugins/screenkey/default.nix
@@ -7,4 +7,19 @@
       (import ./package.nix { inherit lib pkgs; })
     ];
   };
+
+  rootOpts.keymaps = [
+    {
+      mode = "n";
+      key = "<leader>uk";
+      action.__raw = ''
+        function()
+          vim.g.screenkey_statusline_component = not vim.g.screenkey_statusline_component
+          vim.api.nvim_exec_autocmds("User", { pattern = "ScreenkeyToggled" })
+          vim.notify(("screenkey %s"):format(vim.g.screenkey_statusline_component and "on" or "off"))
+        end
+      '';
+      options.desc = "Toggle screenkey";
+    }
+  ];
 }
